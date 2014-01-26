97dfdd2c6381682d65f70808d6f8f40372a75849
Lindsey Smith <lindseyengineer@gmail.com>
2014-01-26 14:31:27 -0800

    fleshed out scraping calls and responses

--- snpapi/scrape.py
+++ snpapi/scrape.py
@@ -1,30 +1,71 @@
 from bs4 import BeautifulSoup
 import urllib
 
-def genotype(rsid):
-  if rsid[0] == 'I' or rsid[0] == 'i':
-    return { 'error': 'Cannot find indicators, must use rs #s'}
-  soup = BeautifulSoup(urllib.urlopen('http://snpedia.com/index.php/Special:Browse/' + rsid).read())
+def getLocations(soup):
+  """
+  requires a base BeatifulSoup object(with url)
+  quick and dirty function to find get the locations of all the table rows and their labels
+  """
+  results = []
   trows = soup('table')[1].find_all('th')
   if len(trows) < 2:
     return { 'error': 'RSID not found.' }
-  for i in range(len(trows)):
-    print trows[i].get_text()
+  for header in trows:
+    cleanText = header.get_text().split('\n')[0].replace(u'\xa0', ' ')
+    results.append(cleanText)
+  return results
 
-def snp(pair, rsid):
-  if rsid[0] == 'I' or rsid[0] == 'i':
-    return { 'error': 'Cannot find indicators, must use rs #s'}
-  soup = BeautifulSoup(urllib.urlopen('http://snpedia.com/index.php/Special:Browse/' + rsid + pair).read())
-  trows = soup('table')[1].find_all('tr')
-  if len(trows) < 2:
-    return { 'error': 'That base pair does not have a trait associated with it.' }
-  chromosome = trows[6].find_all('span')[0].get_text().split()[0]
-  position = ''.join(trows[2].find_all('span')[0].get_text().split()[0].split(','))
-  expression = trows[8].find_all('span')[0].get_text().split(u"\u00A0")[0]
+def getData(locations, soup):
+  """
+  requires the locations array from getLocations and a base BeatifulSoup object(with url)
+  pull genotype info based on
+  """
+  chromeIndex = locations.index('On chromosome')
+  positionIndex = locations.index('Chromosome position')
+  traitIndex = locations.index('Summary')
+
+  tvalues = soup('table')[1].find_all('td')
+  chromosome = tvalues[chromeIndex].find_all('span')[0].get_text().split()[0]
+  position = ''.join(tvalues[positionIndex].find_all('span')[0].get_text().split()[0].split(','))
+  trait = tvalues[traitIndex].find_all('span')[0].get_text().split(u"\u00A0")[0]
   result = {
     'chromosome': chromosome,
     'position': position,
-    'expression': expression,
-    'rsid': rsid,
-    'genotype': bpair
-    }
+    'trait': trait
+  }
+  return result
+
+def genotype(rsid):
+  """
+  requires an rsid string
+  grabs information on an rsid, including location on the chromosome and general traits
+  """
+  if rsid[0] == 'I' or rsid[0] == 'i':
+    return { 'error': 'Cannot find indicators, must use rs #s'}
+  soup = BeautifulSoup(urllib.urlopen('http://snpedia.com/index.php/Special:Browse/' + rsid).read())
+  trows = soup('table')[1].find_all('tr')
+  if len(trows) < 2:
+    return { 'error': 'That rsid does not have any data/does not exist.' }
+  locations = getLocations(soup)
+  genotypeData = getData(locations, soup)
+  genotypeData['rsid'] = rsid
+  return genotypeData
+
+def snp(rsid, pair):
+  """
+  requires rsid and pair string
+  grabs specific information for an snp with the supplied base pair
+  """
+  if rsid[0] == 'I' or rsid[0] == 'i':
+    return { 'error': 'Cannot find indicators, must use rs #s'}
+  formatPair = '(' + pair[0].upper() + ';' + pair[1].upper() + ')'
+  soup = BeautifulSoup(urllib.urlopen('http://snpedia.com/index.php/Special:Browse/' + rsid + formatPair).read())
+  trows = soup('table')[1].find_all('tr')
+  print rsid + formatPair
+  if len(trows) < 2:
+    return { 'error': 'That base pair does not have a trait associated with it.' }
+  locations = getLocations(soup)
+  genotypeData = getData(locations, soup)
+  genotypeData['rsid'] = rsid
+  genotypeData['genotype'] = pair
+  return genotypeData
