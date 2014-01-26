138789be054dd93e7190919fbb5138d26aa93378
Lindsey Smith <lindseyengineer@gmail.com>
2014-01-26 14:56:05 -0800

    syntax error

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from snpapi import app
 from werkzeug.contrib.fixers import ProxyFix
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", (port=int(environ["PORT"]))
+  app.run(host="0.0.0.0", port=int(environ["PORT"]))
