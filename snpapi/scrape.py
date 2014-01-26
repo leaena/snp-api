b1c8962df76f9475d935a2327d6261cb17cabd65
Lindsey Smith <lindseyengineer@gmail.com>
2014-01-26 11:08:14 -0800

    creating scraping functions

--- snpapi/scrape.py
+++ snpapi/scrape.py
@@ -2,15 +2,18 @@ from bs4 import BeautifulSoup
 import urllib
 
 def genotype(rsid):
+  if rsid[0] == 'I' or rsid[0] == 'i':
+    return { 'error': 'Cannot find indicators, must use rs #s'}
   soup = BeautifulSoup(urllib.urlopen('http://snpedia.com/index.php/Special:Browse/' + rsid).read())
   trows = soup('table')[1].find_all('th')
   if len(trows) < 2:
     return { 'error': 'RSID not found.' }
   for i in range(len(trows)):
-    print i
     print trows[i].get_text()
 
 def snp(pair, rsid):
+  if rsid[0] == 'I' or rsid[0] == 'i':
+    return { 'error': 'Cannot find indicators, must use rs #s'}
   soup = BeautifulSoup(urllib.urlopen('http://snpedia.com/index.php/Special:Browse/' + rsid + pair).read())
   trows = soup('table')[1].find_all('tr')
   if len(trows) < 2:
