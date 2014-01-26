35c60ddc3ac7e698b8d6e2c46cee040baa056355
Lindsey Smith <lindseyengineer@gmail.com>
2014-01-26 14:32:14 -0800

    updated to return scraped results

--- snpapi/views.py
+++ snpapi/views.py
@@ -10,14 +10,13 @@ def index():
 def get_snps(rsid):
   # get chromosome, position, trait options
   result = scrape.genotype(rsid)
-  return jsonify( {'nan': '1'} )
+  return jsonify( result )
 
 # gives specific options for an rsid based on a persons basepair
 @app.route('/snp/api/<rsid>/<bpair>', methods = ['GET'])
 def get_trait(rsid, bpair):
   # get chromosome, position, trait
-  pair = '(' + bpair[0] + ';' + bpair[1] + ')'
-  result = scrape.snp(rsid, pair)
+  result = scrape.snp(rsid, bpair)
   return jsonify( result )
 
 @app.errorhandler(404)
